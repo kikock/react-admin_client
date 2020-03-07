afe6a4050a716234d9753882d2d057d97c37a3a9
kikock <kikock@qq.com>
2020-03-07 12:49:30 +0800

    升级模块版本 增加依赖忽略

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //基础模块
 import React, {Component} from 'react';
 // 路由模块
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/login/login';
 import Admin from './pages/admin/admin';
@@ -14,10 +14,10 @@ export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <switch> {/*只匹配其中一个*/}
+                <Switch> {/*只匹配其中一个*/}
                     <Route path='/' component={Login}></Route>
                     <Route path='/admin' component={Admin}></Route>
-                </switch>
+                </Switch>
             </BrowserRouter>
         )
     };
